c57626c8e0f3a09dfa56fe58c0ef794467ca1ea7
Logan Bell <loganbell@gmail.com>
2011-08-06 15:54:52 -0700

    Removed can method ( unnecessary in this context ) and added perldoc.

--- lib/MooseX/Role/MissingMethodUtils.pm
+++ lib/MooseX/Role/MissingMethodUtils.pm
@@ -8,31 +8,69 @@ sub AUTOLOAD
     my ($self, @params)  = @_;
     my ($name)           = our $AUTOLOAD =~ /::(\w+)$/;
 
-    my $meth_ref    = $self->can($name);
+    my $meth_ref        = $self->can('method_missing');
 
-    @_ = ($self, $name, @params);
+    if ( $meth_ref ) 
+    {
+        @_ = ($self, $name, @params);
 
-    goto &{$meth_ref} if $meth_ref;
+        goto &{$meth_ref} if $meth_ref;
+    }
 
     return;
 }
 
-sub can
-{
-    my ($self, $method) = @_;
-
-    my $meth_ref = $self->SUPER::can($method);
-
-    return $meth_ref if $meth_ref;
-
-    $meth_ref = $self->SUPER::can('method_missing');
-
-    return $meth_ref if $meth_ref;
-
-}
-
-
 1;
 
 # ABSTRACT: Getting rid of boilerplate AUTOLOAD stuff
+
+=head1 NAME
+
+MooseX::Role::MissingMethodUtils
+
+=head1 SYNOPSIS
+
+    package Foo;
+    use Moose;
+
+    with 'MooseX::Role::MissingMethodUtils';
+
+    sub method_missing {
+        my ($self, $method_name, @params) = @_;
+
+        if ( $method_name eq 'do_this' ) {
+            Delegator->do_this; 
+        }
+    }
+
+=head1 DESCRIPTION
+
+This role will now introduce a method named method_missing. This method is called via AUTOLOAD as a last
+resort in the calling chain. 
+
+Three parameters will be passed in to help with delegation: ref to self,method name, and parameters.
+
+=head1  METHODS
+
+=head2  AUTOLOAD
+
+Just does all the boilerplate autoloading stuff.
+
+=head1  TODO
+
+Add some other useful methods and filter capabilities.
+
+=head1 AUTHOR
+
+Logan Bell C<< <logan.bell@gmail.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011, Logan Bell C<< <logan.bell@gmail.com> >>
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
 
